7cd8c954b0f22256b779ef2097c772f0541626dc
Benjamin Diaz <benjamin14005@gmail.com>
2022-09-27 11:57:05 -0300

    First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    semi: false,
+    trailingComma: 'none',
+    printWidth: 80,
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true
+}
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,20 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
-};
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: ['next', 'turbo', 'prettier', 'standard'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@next/next/no-html-link-for-pages': 'off',
+        'react/jsx-key': 'off',
+        indent: ['error', 4],
+        'space-before-function-paren': ['error', 'never']
+    }
+}
