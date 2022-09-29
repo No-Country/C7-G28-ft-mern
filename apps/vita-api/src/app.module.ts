603a3b29bec7f7a4ae9c0f24ceb4e368240ede99
Monosen <edinson.bolanos00@usc.edu.co>
2022-09-29 13:31:08 -0500

    adding appointment

--- apps/vita-api/src/app.module.ts
+++ apps/vita-api/src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule,
         UserModule,
-        AuthModule
+        AuthModule,
+        AppointmentModule
     ]
 })
 export class AppModule {}
