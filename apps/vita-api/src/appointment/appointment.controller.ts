7a499b26e8ccf390f23265598551a5adf329c5a2
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-03 04:42:00 -0500

    adding method update appointment

--- apps/vita-api/src/appointment/appointment.controller.ts
+++ apps/vita-api/src/appointment/appointment.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common'
+import {
+    Controller,
+    Post,
+    Body,
+    UseGuards,
+    Get,
+    Patch,
+    Query,
+    Param
+} from '@nestjs/common'
 import { User } from '@prisma/client'
 import { JwtGuard } from '../auth/guard'
 import { GetUser } from '../auth/decorator'
@@ -9,6 +18,12 @@ import { UpdateAppointmentDto } from './dto/update-appointment.dto'
 @Controller('appointment')
 export class AppointmentController {
     constructor(private readonly appointmentService: AppointmentService) {}
+
+    @Patch(':id')
+    @UseGuards(JwtGuard)
+    update(@Param('id') id: string, @Body() data: UpdateAppointmentDto) {
+        return this.appointmentService.updateAppointment(+id, data)
+    }
 
     @Post()
     @UseGuards(JwtGuard)
--- apps/vita-api/src/appointment/appointment.service.ts
+++ apps/vita-api/src/appointment/appointment.service.ts
@@ -1,5 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common'
-import { User } from '@prisma/client'
+import {
+    BadRequestException,
+    Injectable,
+    InternalServerErrorException
+} from '@nestjs/common'
+import { StatusAppointment, User } from '@prisma/client'
 import { PrismaService } from '../prisma/prisma.service'
 import { CreateAppointmentDto } from './dto/create-appointment.dto'
 import { UpdateAppointmentDto } from './dto/update-appointment.dto'
@@ -7,6 +11,25 @@ import { UpdateAppointmentDto } from './dto/update-appointment.dto'
 @Injectable()
 export class AppointmentService {
     constructor(private readonly prisma: PrismaService) {}
+
+    async updateAppointment(id: number, data: UpdateAppointmentDto) {
+        try {
+            const { date, time, statusAppointment = 'PENDING' } = data
+
+            const appointment = await this.prisma.appointment.update({
+                where: { id },
+                data: {
+                    date,
+                    time,
+                    statusAppointment: StatusAppointment[statusAppointment]
+                }
+            })
+
+            return appointment
+        } catch (error) {
+            this.handleExceptions(error)
+        }
+    }
 
     async createAppointment(data: CreateAppointmentDto, user: User) {
         try {
@@ -39,6 +62,9 @@ export class AppointmentService {
     }
 
     private handleExceptions(error: any): never {
+        if (error.code === 'P2025')
+            throw new BadRequestException(error.meta.cause)
+
         console.log(error)
         throw new InternalServerErrorException(`Please check logs`)
     }
