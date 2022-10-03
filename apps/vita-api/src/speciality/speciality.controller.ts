2a71287cbfd72abcf1f69756ba49d62794843ef9
aletomasella <aletomasella09@gmail.com>
2022-10-03 10:28:38 -0300

    Adding routes

--- apps/vita-api/src/speciality/speciality.controller.ts
+++ apps/vita-api/src/speciality/speciality.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { SpecialityDto } from './dto'
+import { SpecialityService } from './speciality.service'
 
-@Controller('speciality')
-export class SpecialityController {}
+@Controller('specialities')
+export class SpecialityController {
+    constructor(private SpecialityService: SpecialityService) {}
+
+    @Get('all')
+    async getAllSpecialities() {
+        return await this.SpecialityService.getAllSpecialities()
+    }
+
+    @Get('one/:name')
+    async getOneSpeciality(@Param('name') name: string) {
+        return await this.SpecialityService.getOneSpeciality(name)
+    }
+
+    @Post('create')
+    async createSpeciality(@Body() speciality: SpecialityDto) {
+        return await this.SpecialityService.createSpeciality(speciality)
+    }
+}
--- apps/vita-api/src/speciality/speciality.service.ts
+++ apps/vita-api/src/speciality/speciality.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { SpecialityDto } from './dto'
 
 @Injectable()
-export class SpecialityService {}
+export class SpecialityService {
+    constructor(prisma: PrismaService) {}
+
+    getAllSpecialities() {
+        return [
+            { id: 1, name: 'Speciality 1' },
+            { id: 2, name: 'Speciality 2' },
+            { id: 3, name: 'Speciality 3' }
+        ]
+    }
+
+    getOneSpeciality(name: string) {
+        return { id: 1, name: 'Speciality 1' }
+    }
+
+    createSpeciality(speciality: SpecialityDto) {
+        return { speciality }
+    }
+}
