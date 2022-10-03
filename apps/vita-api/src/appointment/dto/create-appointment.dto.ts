584de41340cf003753b7cd54330f0df4a0760766
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-03 03:28:15 -0500

    adding create-appointment.dto and update-appointment.dto

--- apps/vita-api/src/appointment/dto/create-appointment.dto.ts
+++ apps/vita-api/src/appointment/dto/create-appointment.dto.ts
@@ -1,1 +1,40 @@
-export class CreateAppointmentDto {}
+import {
+    IsEnum,
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsPositive,
+    IsString
+} from 'class-validator'
+
+export enum Status {
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE'
+}
+
+enum StatusAppointment {
+    PENDING = 'PENDING',
+    APPROVED = 'APPROVED',
+    CANCELED = 'CANCELED'
+}
+export class CreateAppointmentDto {
+    @IsNumber()
+    @IsPositive()
+    doctorId: number
+
+    @IsNotEmpty()
+    @IsString()
+    date: string
+
+    @IsNotEmpty()
+    @IsString()
+    time: string
+
+    @IsEnum(StatusAppointment)
+    @IsOptional()
+    statusAppointment?: string
+
+    @IsEnum(Status)
+    @IsOptional()
+    status?: string
+}
