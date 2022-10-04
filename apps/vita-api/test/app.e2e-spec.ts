2a22ea3c96ca4ccaac44c8493889247540cd2f13
aletomasella <aletomasella09@gmail.com>
2022-10-04 16:36:03 -0300

    test(vita-api): setting up everything for End To End Testing

--- apps/vita-api/test/app.e2e-spec.ts
+++ apps/vita-api/test/app.e2e-spec.ts
@@ -1,11 +1,29 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { AppModule } from '../src/app.module'
 
 describe('App End to End Test', () => {
+    let app: INestApplication
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule]
         }).compile()
+        app = moduleRef.createNestApplication()
+
+        app.setGlobalPrefix('api')
+
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+                forbidNonWhitelisted: true
+            })
+        )
+
+        await app.init()
+    })
+
+    afterAll(async () => {
+        app.close()
     })
     it.todo('should pass')
 })
