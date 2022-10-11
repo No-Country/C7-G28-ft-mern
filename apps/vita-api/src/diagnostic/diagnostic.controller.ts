2b46180240c19458232db221bfaf588fd3bbe751
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-11 12:15:36 -0500

    feat(api/diagnostic): protecting routes by user role

--- apps/vita-api/src/diagnostic/diagnostic.controller.ts
+++ apps/vita-api/src/diagnostic/diagnostic.controller.ts
@@ -5,37 +5,51 @@ import {
     Body,
     Patch,
     Param,
-    Delete
+    Delete,
+    UseGuards
 } from '@nestjs/common'
+import { RolesGuard } from '../auth/guard/roles.guard'
+import { JwtGuard } from '../auth/guard'
 import { DiagnosticService } from './diagnostic.service'
 import { CreateDiagnosticDto } from './dto/create-diagnostic.dto'
 import { UpdateDiagnosticDto } from './dto/update-diagnostic.dto'
+import { Roles } from '../auth/decorator/roles.decorator'
 
 @Controller('diagnostic')
 export class DiagnosticController {
     constructor(private readonly diagnosticService: DiagnosticService) {}
 
     @Post()
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles('DOCTOR')
     create(@Body() data: CreateDiagnosticDto) {
         return this.diagnosticService.create(data)
     }
 
     @Get()
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles('DOCTOR')
     findAll() {
         return this.diagnosticService.findAllDiagnostics()
     }
 
     @Get(':id')
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles('DOCTOR', 'PATIENT')
     findOne(@Param('id') id: string) {
         return this.diagnosticService.findOneDiagnostic(+id)
     }
 
     @Patch(':id')
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles('DOCTOR')
     update(@Param('id') id: string, @Body() data: UpdateDiagnosticDto) {
         return this.diagnosticService.updateDiagnostic(+id, data)
     }
 
     @Delete(':id')
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles('DOCTOR')
     remove(@Param('id') id: string) {
         return this.diagnosticService.deleteDiagnostic(+id)
     }
