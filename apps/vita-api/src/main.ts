66a7cbbd182d39fa39b2fd777d99aa22e15b1799
aletomasella <aletomasella09@gmail.com>
2022-10-12 10:54:02 -0300

    docs(vita-api): adding Swagger to our API

--- apps/vita-api/src/main.ts
+++ apps/vita-api/src/main.ts
@@ -2,13 +2,11 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     const logger = new Logger('main')
-
-    const PORT = process.env.PORT || 3001
-
     app.setGlobalPrefix('api')
 
     app.useGlobalPipes(
@@ -17,6 +15,19 @@ async function bootstrap() {
             forbidNonWhitelisted: true
         })
     )
+
+    const config = new DocumentBuilder()
+        .setTitle('Vita API')
+        .setDescription('The Vita API description')
+        .setVersion('1.0')
+        .addTag('vita')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+
+    SwaggerModule.setup('api/docs', app, document)
+
+    const PORT = process.env.PORT || 3001
 
     await app.listen(PORT)
     logger.log(`Application listening on port ${PORT}`)
