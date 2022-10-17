24c608a01331be2b9ecdc8495e633db165018f65
Benjamin Diaz <benjamin14005@gmail.com>
2022-10-16 21:08:21 -0300

    feat(web/lib): added mock data for home page components
    n

---
+++ apps/vita-web/lib/data/frequentlyQuestionsData.ts
@@ -0,0 +1,17 @@
+export const frequentlyQuestionsData = [
+    {
+        title: 'Can my family access my portal?',
+        content:
+            'Yes, your family can access your portal. They will need to create an account and then you can add them as a family member. You can also add them as a family member when you create your account.'
+    },
+    {
+        title: 'Is my information safe?',
+        content:
+            'Yes, your information is safe. We use the latest security technology to keep your information safe. We also use the latest encryption technology to keep your information safe. '
+    },
+    {
+        title: 'How do I log into the portal?',
+        content:
+            'You can log into the portal by clicking on the login button on the top right of the page. You will then be prompted to enter your email and password. If you do not have an account, you can create one by clicking on the create account button.'
+    }
+]
