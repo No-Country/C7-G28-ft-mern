c7393fda4a01b730602081a59bc045647b52c289
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-17 00:54:23 -0500

    feat(api/file): protecting img upload path

--- apps/vita-api/src/file/file.controller.ts
+++ apps/vita-api/src/file/file.controller.ts
@@ -2,18 +2,24 @@ import {
     Controller,
     Post,
     UploadedFiles,
+    UseGuards,
     UseInterceptors
 } from '@nestjs/common'
 import { FileService } from './file.service'
 import { FilesInterceptor } from '@nestjs/platform-express'
 import { diskStorage } from 'multer'
 import { fileName, fileFilter } from './helpers'
+import { RolesGuard, JwtGuard } from '../auth/guard'
+import { Role } from '@prisma/client'
+import { Roles } from '../auth/decorator/roles.decorator'
 
 @Controller('files')
 export class FileController {
     constructor(private readonly fileService: FileService) {}
 
     @Post('upload')
+    @UseGuards(JwtGuard, RolesGuard)
+    @Roles(Role.DOCTOR)
     @UseInterceptors(
         FilesInterceptor('files', 3, {
             fileFilter,
