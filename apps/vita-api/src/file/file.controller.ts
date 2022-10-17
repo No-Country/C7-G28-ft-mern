dee4ca6bee5ed352afa2f8952e3acbaea7dc4c69
Monosen <edinson.bolanos00@usc.edu.co>
2022-10-17 00:48:58 -0500

    feat(api/file): adding functionality to upload images to cloudinary

--- apps/vita-api/src/file/file.controller.ts
+++ apps/vita-api/src/file/file.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { FileService } from './file.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import {
+    Controller,
+    Post,
+    UploadedFiles,
+    UseInterceptors
+} from '@nestjs/common'
+import { FileService } from './file.service'
+import { FilesInterceptor } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
+import { fileName, fileFilter } from './helpers'
 
-@Controller('file')
+@Controller('files')
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+    constructor(private readonly fileService: FileService) {}
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.fileService.create(createFileDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.fileService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fileService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.fileService.update(+id, updateFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fileService.remove(+id);
-  }
+    @Post('upload')
+    @UseInterceptors(
+        FilesInterceptor('files', 3, {
+            fileFilter,
+            storage: diskStorage({ filename: fileName })
+        })
+    )
+    uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+        return this.fileService.uploadImagesToCloudinary(files)
+    }
 }
--- apps/vita-api/src/file/file.service.ts
+++ apps/vita-api/src/file/file.service.ts
@@ -1,26 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import { Injectable } from '@nestjs/common'
+import { v2 as cloudinary } from 'cloudinary'
 
 @Injectable()
 export class FileService {
-  create(createFileDto: CreateFileDto) {
-    return 'This action adds a new file';
-  }
+    async uploadImagesToCloudinary(files: Express.Multer.File[]) {
+        const imgs = files.map(async file => {
+            return await this.saveFile(file)
+        })
 
-  findAll() {
-    return `This action returns all file`;
-  }
+        const imgsUrl = await Promise.all(imgs)
 
-  findOne(id: number) {
-    return `This action returns a #${id} file`;
-  }
+        return { urls: imgsUrl }
+    }
 
-  update(id: number, updateFileDto: UpdateFileDto) {
-    return `This action updates a #${id} file`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} file`;
-  }
+    async saveFile(file: Express.Multer.File): Promise<string> {
+        const { secure_url: secureUrl } = await cloudinary.uploader.upload(
+            file.path,
+            { folder: 'test' }
+        )
+        return secureUrl
+    }
 }
